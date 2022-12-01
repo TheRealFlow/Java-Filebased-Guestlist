b879ef8e296fbdd9c02ff64453c889c7a923a95a
Florian Ludwig <florian.ludwig-hro@gmx.de>
2022-12-01 10:56:30 +0100

    Update and Refactor

--- src/main/java/de/neuefische/GuestList.java
+++ src/main/java/de/neuefische/GuestList.java
@@ -1,17 +1,29 @@
 package de.neuefische;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class GuestList {
-    List<String> guests;
-
-    public void setGuests(List<String> guests) {
-        this.guests = guests;
+    public void setGuests (List<String> guests) throws IOException {
+        Path path = Paths.get("guests.txt");
+        String guestsTxt = String.join("\n", guests);
+        Files.write(path, guestsTxt.getBytes());
     }
 
-    public List<String> getGuests() {
-        return this.guests;
+    public List<String> getGuests () throws IOException {
+        Path path = Paths.get("guests.txt");
+        return Files.readAllLines(path);
     }
 
+    public void addGuest (String guest) throws IOException {
+        Files.write(
+                Paths.get("guests.txt"),
+                ("\n" + guest).getBytes(),
+                StandardOpenOption.APPEND
+        );
+    }
 }
